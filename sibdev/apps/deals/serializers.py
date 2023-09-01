32b065964a1f35e9fde3e7ca4a75938b568d4719
Nurmukhamed1 <nurmaduisenbayev@gmail.com>
2023-09-01 14:14:18 +0600

    refactoring & cleaning code: logic of gems to serializer

--- sibdev/apps/deals/serializers.py
+++ sibdev/apps/deals/serializers.py
@@ -1,12 +1,13 @@
+from django.db.models import Sum, Count
 from rest_framework import serializers
 
 from apps.deals.models import Deals
 
 
 class DealsSerializer(serializers.ModelSerializer):
-    username = serializers.CharField()
+    username = serializers.CharField(source="customer")
     spent_money = serializers.IntegerField()
-    gems = serializers.ListSerializer(child=serializers.CharField())
+    gems = serializers.SerializerMethodField()
 
     class Meta:
         model = Deals
@@ -15,3 +16,19 @@ class DealsSerializer(serializers.ModelSerializer):
             "spent_money",
             "gems",
         )
+
+    def get_gems(self, obj):
+        queryset = Deals.objects.all()
+        top_clients = queryset.values('customer').annotate(spent_money=Sum('total')).order_by('-spent_money')[:5]
+
+        gems_of_top_clients = (
+            queryset.filter(customer__in=top_clients.values_list('customer', flat=True))
+            .values('item').
+            annotate(customer_count=Count('customer', distinct=True))
+            .filter(customer_count__gte=2)
+        )
+
+        return (queryset.filter(customer=obj["customer"]).
+                filter(item__in=gems_of_top_clients.values_list("item", flat=True))
+                .distinct('item').
+                values_list("item", flat=True))
--- sibdev/apps/deals/views/deals.py
+++ sibdev/apps/deals/views/deals.py
@@ -1,4 +1,4 @@
-from django.db.models import Sum, Count
+from django.db.models import Sum
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
@@ -12,30 +12,10 @@ from apps.deals.serializers import DealsSerializer
 class DealsViewSet(viewsets.GenericViewSet):
     filter_backends = (DjangoFilterBackend,)
     queryset = Deals.objects.all()
-
     serializer_class = DealsSerializer
 
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
-        top_clients = queryset.values('customer').annotate(spent_money=Sum('total')).order_by('-spent_money')[:5]
-        gems_of_top_clients = (
-            queryset.filter(customer__in=top_clients.values_list('customer', flat=True))
-            .values('item').
-            annotate(customer_count=Count('customer', distinct=True))
-            .filter(customer_count__gte=2)
-        )
-
-        client_data = []
-        for client in top_clients:
-            obj = {
-                "username": client["customer"],
-                "spent_money": client["spent_money"],
-                "gems": [],
-            }
-            gems = queryset.filter(customer=client['customer']).filter(
-                item__in=gems_of_top_clients.values_list("item", flat=True)).distinct('item')
-            for gem in gems:
-                obj['gems'].append(gem.item)
-            client_data.append(obj)
-        serializer = self.get_serializer(client_data, many=True)
+        queryset = queryset.values('customer').annotate(spent_money=Sum('total')).order_by('-spent_money')[:5]
+        serializer = self.get_serializer(queryset, many=True)
         return Response({"response": serializer.data})
